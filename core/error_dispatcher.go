d26c2671c9d5afe2257b6636caf3056fe1b79c52
emonuh <emonuh20@gmail.com>
2017-03-29 13:40:38 +0900

    fixed

--- core/error_dispatcher.go
+++ core/error_dispatcher.go
@@ -4,51 +4,40 @@ import "go.uber.org/zap/zapcore"
 
 func NewErrorDispatcher(base zapcore.Core, err zapcore.Core) zapcore.Core {
 	return &errorDispatcher{
-		base: base,
+		Core: base,
 		err:  err,
 	}
 }
 
 type errorDispatcher struct {
-	base zapcore.Core
-	err  zapcore.Core
+	zapcore.Core
+	err zapcore.Core
 }
 
 func (e *errorDispatcher) With(fields []zapcore.Field) zapcore.Core {
 	clone := e.clone()
-	clone.base = e.base.With(fields)
+	clone.Core = e.Core.With(fields)
 	clone.err = e.err.With(fields)
 	return clone
-}
-
-func (e *errorDispatcher) Enabled(lvl zapcore.Level) bool {
-	return e.base.Enabled(lvl)
 }
 
 func (e *errorDispatcher) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.CheckedEntry {
 	if ent.Level >= zapcore.ErrorLevel {
 		return e.err.Check(ent, ce)
 	}
-	return e.base.Check(ent, ce)
-}
-
-func (e *errorDispatcher) Write(ent zapcore.Entry, fields []zapcore.Field) error {
-	if ent.Level >= zapcore.ErrorLevel {
-		return e.err.Write(ent, fields)
-	}
-	return e.base.Write(ent, fields)
+	return e.Core.Check(ent, ce)
 }
 
 func (e *errorDispatcher) Sync() error {
 	if err := e.err.Sync(); err != nil {
 		return err
 	}
-	return e.base.Sync()
+	return e.Core.Sync()
 }
 
 func (e *errorDispatcher) clone() *errorDispatcher {
 	return &errorDispatcher{
-		base: e.base,
+		Core: e.Core,
 		err:  e.err,
 	}
 }
